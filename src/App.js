1fa27156c9599c291119864893a028173515c8a3
andresantana <santana.agr@gmail.com>
2021-08-02 12:15:13 -0300

    added skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from "./logo.svg";
 import "./App.css";
+import MyButton from "./MyButton";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div>Giant Calculator</div>
       </header>
+      <main>
+        <div className="calculator">
+          <div className="row1">
+            <input />
+            <MyButton titulo="C" />
+          </div>
+          <div className="row2">
+            <MyButton titulo="9" />
+            <MyButton titulo="8" />
+            <MyButton titulo="7" />
+            <MyButton titulo="/" />
+          </div>
+          <div className="row3">
+            <MyButton titulo="6" />
+            <MyButton titulo="5" />
+            <MyButton titulo="4" />
+            <MyButton titulo="*" />
+          </div>
+          <div className="row4">
+            <MyButton titulo="3" />
+            <MyButton titulo="2" />
+            <MyButton titulo="1" />
+            <MyButton titulo="-" />
+          </div>
+          <div className="row5">
+            <MyButton titulo="." />
+            <MyButton titulo="0" />
+            <MyButton titulo="=" />
+            <MyButton titulo="+" />
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/MyButton.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MyButton(props) {
+  return <button>{props.titulo}</button>;
+}
